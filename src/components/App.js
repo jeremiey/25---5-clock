bb8a37719c6297774925bded7b113604e3cc6519
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2022-02-23 09:26:30 +0100

    created function components and rendered then

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import '../App.scss'
+import BreakInterval from './BreakInterval'
+import SessionLength from './SessionLength'
 
 class App extends React.Component {
   constructor() {
     super()
 
-    this.state {
-      
+    this.state = {
+      breakLength: 5,
+      sessionLength: 25
     }
   }
 
@@ -14,6 +17,8 @@ class App extends React.Component {
     return (
       <main>
         <h2>Pomodoro Clock</h2>
+        <BreakInterval  breakInterval={this.state.breakLength} />
+        <SessionLength sessionLength={this.state.sessionLength} />
       </main>
     )
   }
