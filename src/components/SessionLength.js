ebef485bb7ccfc66379deb1fa547f5f2be0940f0
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2022-02-24 18:28:40 +0100

    removed all semi-colons and made some formattings

--- src/components/SessionLength.js
+++ src/components/SessionLength.js
@@ -2,30 +2,34 @@ import React from "react"
 
 function SessionLength(props) {
 
-	function decreaseCounter() {
-		if(props.sessionLength === 1) {
-			return
-		}
-		props.decreaseSession()
-	}
+  function decreaseCounter() {
+    if(props.sessionLength === 1) {
+      return
+    }
+    props.decreaseSession()
+  }
 
-	function increaseCounter() {
-		if(props.sessionLength === 60) {
-			return
-		}
-		props.increaseSession()
-	}
-	
-	return (
-		<section>
-			<h4>Session Length</h4>
-			<section className="interval-container">
-				<button onClick={decreaseCounter}>Down</button>
-				<p className="interval-length">{props.sessionLength}</p>
-				<button onClick={increaseCounter}>Up</button>
-			</section>
-		</section>
-	)
+  function increaseCounter() {
+    if(props.sessionLength === 60) {
+      return
+    }
+    props.increaseSession()
+  }
+
+  return (
+    <section>
+      <h4>Session Length</h4>
+      <section className="interval-container">
+        <button onClick={decreaseCounter}>
+          Down
+        </button>
+        <p className="interval-length">{props.sessionLength}</p>
+        <button onClick={increaseCounter}>
+          Up
+        </button>
+      </section>
+    </section>
+  )
 }
 
 export default SessionLength
